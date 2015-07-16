4400e67bb201777f57d13da90df9966d1a8be06d
Hari Subbaraj <harisubbaraj@haris-air.gateway.pace.com>
2015-07-16 12:22:03 -0700

    blog navbar scrolling issue fixed

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php get_header(); ?>
 
-	<div class="container-fluid">
+	<div class="container-fluid" id="body-main">
 		<main role="main">
 		<!-- section -->
 			<section class="featured_articles">
+			<div class="container-fluid">
+				<div class="row-fluid">
 				<h1><?php //_e( 'Latest Posts', 'wearhaus' ); ?></h1>
 				<div class="large-img">
 
@@ -12,7 +14,8 @@
 				<?php get_template_part('loop'); ?>
 
 				<?php get_template_part('pagination'); ?>
-
+			</div class="row-fluid">
+			</div>
 			</section>
 		<!-- /section -->
 		</main>
--- js/scripts.js
+++ js/scripts.js
@@ -8,9 +8,11 @@
         $(window).scroll(function(){
                 if( $(window).scrollTop() > stickyHeaderTop ) {
                         $('#stickyheader').css({position: 'fixed', top: '0px', width:'100%'});
+                        $('#body-main').css({'padding-top': '50px'});
                         //$('#stickyalias').css('display', 'block');
                 } else {
                         $('#stickyheader').css({position: 'static', top: '0px'});
+                        $('#body-main').css({'padding-top': '0px'});
                         //$('#stickyalias').css('display', 'none');
                 }
         });
