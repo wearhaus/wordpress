433d0fee64effd78f7e62034e29982aee3ff17ab
Ken Lauguico <ken@wearhaus.com>
2015-10-29 15:47:06 -0700

    fixed the messed up tag page

--- tag.php
+++ tag.php
@@ -1,19 +1,24 @@
 <?php get_header(); ?>
 
-	<main role="main">
-		<!-- section -->
-		<section>
-
-			<h1><?php _e( 'Tag Archive: ', 'html5blank' ); echo single_tag_title('', false); ?></h1>
-
-			<?php get_template_part('loop'); ?>
-
-			<?php get_template_part('pagination'); ?>
-
-		</section>
-		<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
-
+<body>
+	<div class="page-main" id="body-main">
+		<div class="container-fluid">
+				<div class="row-fluid">
+					<div class="col-md-8 cat_post_content" id="post_content ">
+						<div id="search_content">
+							<h1 id="post_title">
+								<?php _e( 'Tag Archive: ', 'html5blank' ); echo single_tag_title('', false); ?></h1>
+							</h1>
+							
+							<?php get_template_part('loop'); ?>
+							
+							<?php get_template_part('pagination'); ?>
+						</div>
+					</div>
+					<?php get_sidebar(); ?>
+				</div>
+			</div>
+		</div>
+	</div>
 <?php get_footer(); ?>
+</body>
