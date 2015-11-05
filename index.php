59b9ab7af5cd71dccf7dbd5885c3594ffa80d3c6
Richie Zeng <richzeng@gmail.com>
2015-11-04 16:25:12 -0800

    Made twitter widget shorter. From 600px to 400px

--- index.php
+++ index.php
@@ -184,8 +184,8 @@
 					</div>
 				</div>
 				<div class="sidebar_body" id="tw_sb_body">
-					<a class="twitter-timeline" href="https://twitter.com/wearhausinc" data-widget-id="624299359467745280">Tweets by @wearhausinc</a>
-					<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+                    <a class="twitter-timeline" href="https://twitter.com/wearhausinc" data-widget-id="662062697186390016">Tweets by @wearhausinc</a>
+                    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 				</div>
 				<div class="sidebar_header" id="ig_sb">
 					<div class="text-container">
