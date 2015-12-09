8b1a9d9e6d8019a21b1112dac8f775e75e97ddb4
Ken Lauguico <ken@wearhaus.com>
2015-12-09 10:47:22 -0800

    refactored page.php and modified it to let it return more featured posts

--- page.php
+++ page.php
@@ -3,96 +3,48 @@
 	header('Access-Control-Allow-Origin: *');
 	header('Access-Control-Allow-Methods: GET, POST');  
 	headers_list();
-	//Featured Article 1
-	$original_query = $wp_query;
-	$wp_query = null;
-	$args=array('posts_per_page'=>1, 'tag' => 'featured_1');
-	$wp_query = new WP_Query( $args );
-	if ( have_posts() ) :
-					    while (have_posts()) : the_post();
-	$title_1 = get_the_title();
-	$url_1 = get_the_permalink(); 
 
-	if (function_exists('has_post_thumbnail')) {
-	    if ( has_post_thumbnail() ) {
-	    	$thumb_id = get_post_thumbnail_id();
+	$currentFeature = 1;
+	$currentPost = 1;
+	$featureCount = 7;
+	$returnArray = array();
 
+	while ($currentFeature <= $featureCount) {
+		$original_query = $wp_query;
+		$wp_query = null;
+		$args=array('posts_per_page'=>3, 'tag' => 'featured_' . $currentFeature);
+		$wp_query = new WP_Query( $args );
 
-			$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'large', true);
+		if ( have_posts() ) : while (have_posts()) : the_post();
 
-			$thumb_url_1 = $thumb_url_array[0];
-	    }
-	}
-	endwhile;
-	endif;
-	$wp_query = null;
-	$wp_query = $original_query;
+		$title = get_the_title();
+		$url = get_the_permalink(); 
 
-	//Featured Article 2
-	$original_query = $wp_query;
-	$wp_query = null;
-	$args=array('posts_per_page'=>1, 'tag' => 'featured_2');
-	$wp_query = new WP_Query( $args );
-	if ( have_posts() ) :
-					    while (have_posts()) : the_post();
-	$title_2 = get_the_title();
-	$url_2 = get_the_permalink(); 
-	if (function_exists('has_post_thumbnail')) {
-	    if ( has_post_thumbnail() ) {
-	    	$thumb_id = get_post_thumbnail_id();
-			$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'large', true);
-			$thumb_url_2 = $thumb_url_array[0];
-	    }
-	}
-	endwhile;
-	endif;
-	$wp_query = null;
-	$wp_query = $original_query;
-	//Featured Article 3
-	$original_query = $wp_query;
-	$wp_query = null;
-	$args=array('posts_per_page'=>1, 'tag' => 'featured_3');
-	$wp_query = new WP_Query( $args );
-	if ( have_posts() ) :
-					    while (have_posts()) : the_post();
-	$title_3 = get_the_title();
-	$url_3 = get_the_permalink(); 
-	if (function_exists('has_post_thumbnail')) {
-	    if ( has_post_thumbnail() ) {
-	    	$thumb_id = get_post_thumbnail_id();
-			$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'large', true);
-			$thumb_url_3 = $thumb_url_array[0];
-	    }
-	}
-	endwhile;
-	endif;
-	$wp_query = null;
-	$wp_query = $original_query;
+		if (function_exists('has_post_thumbnail')) {
+		    if ( has_post_thumbnail() ) {
+		    	$thumb_id = get_post_thumbnail_id();
+				$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'large', true);
+				$thumb_url = $thumb_url_array[0];
+		    }
+		}
+
+		array_push($returnArray, array(
+		"featured_" . $currentPost => array(
+			"title" => $title,
+			"url" => $url,
+			"img_url" => $thumb_url
+			)));
+
+		$currentPost++;
+
+		endwhile;
+		endif;
+
+		$currentFeature++;
+	};
 
 //Creating JSON object
-$json = json_encode(array(
-
-     "featured_1" => array(
-
-        "title" => $title_1,
-        "url" => $url_1,
-        "img_url" => $thumb_url_1
-     ),
-
-     "featured_2" => array(
-
-        "title" => $title_2,
-        "url" => $url_2,
-        "img_url" => $thumb_url_2
-     ),
-
-     "featured_3" => array(
-
-        "title" => $title_3,
-        "url" => $url_3,
-        "img_url" => $thumb_url_3
-     )
-));
+$json = json_encode($returnArray);
 
 echo $json;
 
