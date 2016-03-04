60ecc0c49aece2d6ee4f944eea743065d81e414d
Ken Lauguico <ken@wearhaus.com>
2016-03-04 12:54:30 -0800

    fixed search results, added new google analytics code

--- loop.php
+++ loop.php
@@ -3,26 +3,29 @@
 	<!-- article -->
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-		<!-- post thumbnail -->
-		<!--<?php //if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
-			<a href="<?php //the_permalink(); ?>" title="<?php the_title(); ?>">
-				<?php //the_post_thumbnail(array(120,120)); // Declare pixel size you need inside the array ?>
-			</a>
-		<?php //endif; ?>-->
-		<!-- /post thumbnail -->
+		<div id="search-th">
+			<!-- post thumbnail -->
+			<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
+				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+					<?php the_post_thumbnail(array(120,120)); // Declare pixel size you need inside the array ?>
+				</a>
+			<?php endif; ?>
+			<!-- /post thumbnail -->
+		</div>
+		<div id="search-body">
+			<!-- post title -->
+			<h2>
+				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+			</h2>
+			<!-- /post title -->
 
-		<!-- post title -->
-		<h2>
-			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-		</h2>
-		<!-- /post title -->
+			<!-- post details -->
+			<span class="date"><?php the_time('F j, Y'); ?> <?php //the_time('g:i a'); ?></span>
+			<span class="author"><?php //_e( 'Published by', 'html5blank' ); ?> <?php//the_author_posts_link(); ?></span>
+			<!-- /post details -->
 
-		<!-- post details -->
-		<span class="date"><?php the_time('F j, Y'); ?> <?php //the_time('g:i a'); ?></span>
-		<span class="author"><?php //_e( 'Published by', 'html5blank' ); ?> <?php//the_author_posts_link(); ?></span>
-		<!-- /post details -->
-
-		<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
+			<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
+		</div>
 
 		
 
