65feb27b7a4a21130a95ab1b3b556e83482961e3
Ken Lauguico <ken@wearhaus.com>
2016-03-07 15:10:56 -0800

    update facebook pixels code

--- header.php
+++ header.php
@@ -51,10 +51,10 @@
     t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
     document,'script','//connect.facebook.net/en_US/fbevents.js');
 
-    fbq('init', '469651369902486');
+    fbq('init', '240641422936912');
     fbq('track', "PageView");</script>
     <noscript><img height="1" width="1" style="display:none"
-    src="https://www.facebook.com/tr?id=469651369902486&ev=PageView&noscript=1"
+    src="https://www.facebook.com/tr?id=240641422936912&ev=PageView&noscript=1"
     /></noscript>
     <!-- End Facebook Pixel Code -->
 
