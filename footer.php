eac73b8d3780216264622e0360dbae476c576f7f
Richie Zeng <richzeng@gmail.com>
2016-06-14 12:03:29 -0700

    Added Onetag to footer

--- footer.php
+++ footer.php
@@ -72,5 +72,69 @@
 		ga('send', 'pageview');
 		</script>
 
+        <!------- Tags for Wearhaus oneTag------->
+
+        <!-- One Tag Conditional Container: Wearhaus  (8448) | Wearhaus oneTag (6579) -->
+
+        <script type="text/javascript">
+        var ft_onetag_6579 = {
+            ft_vars:{
+                "ftXRef":"",
+                "ftXValue":"",
+                "ftXType":"",
+                "ftXName":"",
+                "ftXNumItems":"",
+                "ftXCurrency":"",
+                "U1":"",
+                "U2":"",
+                "U3":"",
+                "U4":"",
+                "U5":"",
+                "U6":"",
+                "U7":"",
+                "U8":"",
+                "U9":"",
+                "U10":"",
+                "U11":"",
+                "U12":"",
+                "U13":"",
+                "U14":"",
+                "U15":"",
+                "U16":"",
+                "U17":"",
+                "U18":"",
+                "U19":"",
+                "U20":""
+                },
+            ot_dom:document.location.protocol+'//servedby.flashtalking.com',
+            ot_path:'/container/8448;62797;6579;iframe/?',
+            ot_href:'ft_referrer='+escape(document.location.href),
+            ot_rand:Math.random()*1000000,
+            ot_ref:document.referrer,
+            ot_init:function(){
+                var o=this,qs='',count=0,ns='';
+                for(var key in o.ft_vars){
+                    qs+=(o.ft_vars[key]==''?'':key+'='+o.ft_vars[key]+'&');
+                }
+                count=o.ot_path.length+qs.length+o.ot_href+escape(o.ot_ref).length;
+                ns=o.ot_ns(count-2000);
+                document.write('<iframe style="position:absolute; visibility:hidden; width:1px; height:1px;" src="'+o.ot_dom+o.ot_path+qs+o.ot_href+'&ns='+ns+'&cb='+o.ot_rand+'"></iframe>');
+            },
+            ot_ns:function(diff){
+                if(diff>0){
+                    var o=this,qo={},
+                        sp=/(?:^|&)([^&=]*)=?([^&]*)/g,
+                        fp=/^(http[s]?):\/\/?([^:\/\s]+)\/([\w\.]+[^#?\s]+)(.*)?/.exec(o.ot_ref),
+                        ro={h:fp[2],p:fp[3],qs:fp[4].replace(sp,function(p1,p2,p3){if(p2)qo[p2]=[p3]})};
+                    return escape(ro.h+ro.p.substring(0,10)+(qo.q?'?q='+unescape(qo.q):'?p='+unescape(qo.p)));
+                }else{
+                    var o=this;
+                    return escape(unescape(o.ot_ref));
+                }
+                    }
+            }
+        ft_onetag_6579.ot_init();
+        </script>
+
 	</body>
 </html>
